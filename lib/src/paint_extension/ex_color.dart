598785578a8dfab42edb2fe93a0e7bf5a10d90c2
StarLit <121178583+StarkLit@users.noreply.github.com>
2025-10-20 23:58:36 +0800

    hos fix

--- lib/src/paint_extension/ex_color.dart
+++ lib/src/paint_extension/ex_color.dart
@@ -19,8 +19,8 @@ Color? stringToColor(String data) {
 }
 
 int colorToInt(Color color) {
-  return (color.a * 255).toInt() << 24 |
-      (color.r * 255).toInt() << 16 |
-      (color.g * 255).toInt() << 8 |
-      (color.b * 255).toInt();
+  return (color.alpha * 255).toInt() << 24 |
+      (color.red * 255).toInt() << 16 |
+      (color.green * 255).toInt() << 8 |
+      (color.blue * 255).toInt();
 }
